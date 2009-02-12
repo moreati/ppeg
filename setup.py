666f336ffa54164dfd647c354348723ccf43f109
Paul Moore <pfmoore@users.noreply.github.com>
2009-02-12 17:43:19 +0000

    Added _cpeg extension

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from distutils.core import setup, Extension
 setup (
     name = 'ppeg',
     version = '1.0',
-    ext_modules = [Extension('_ppeg', ['_ppeg.c'])],
+    ext_modules = [Extension('_ppeg', ['_ppeg.c']),
+                   Extension('_cpeg', ['_cpeg.c'])],
 )
 
--- time_test.py
+++ time_test.py
@@ -8,7 +8,7 @@ pat = Pattern.Dummy()
 pat.dump()
 """
 
-t = timeit.Timer(stmt='print pat(text)', setup=setup)
+t = timeit.Timer(stmt='print pat(text).pos', setup=setup)
 N=5
 print "Time taken: %.2f sec" % (t.timeit(N)/float(N))
 
