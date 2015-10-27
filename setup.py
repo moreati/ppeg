9e4d08258225211f37f5652d7ac4ea3ab70f5c7a
Alex Willmer <alex@moreati.org.uk>
2015-10-27 15:31:40 +0000

    Add PyPI metadata

--- setup.py
+++ setup.py
@@ -2,8 +2,29 @@ from distutils.core import setup, Extension
 
 setup (
     name = 'ppeg',
-    version = '1.0',
+    version='0.9',
+    description="A Python port of Lua's LPeg pattern matching library",
+    url='https://bitbucket.org/pmoore/ppeg',
+
+    author='Paul Moore',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2 :: Only',
+        'Topic :: Text Processing :: General',
+    ],
+
+    keywords='parsing peg grammar regex',
+
     ext_modules = [Extension('_ppeg', ['_ppeg.c']),
                    Extension('_cpeg', ['_cpeg.c'])],
+    py_modules=[
+        'PythonImpl',
+        'pegmatcher',
+    ],
 )
 
