542a7e88e8a6022bf4fa1e03dcea0ce586d3b4d1
Alex Willmer <alex@moreati.org.uk>
2015-10-28 16:18:53 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname, encoding='utf-8'):
 
 setup (
     name='PPeg',
-    version='0.9.1',
+    version='0.9.2',
     description="A Python port of Lua's LPeg pattern matching library",
     long_description=read('README.rst'),
     url='https://github.com/moreati/ppeg',
